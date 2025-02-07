2778dc1f2162309798248d7f74740049ae7a4d7e
MattB <mattb-msft@hotmail.com>
2025-02-07 12:28:07 -0800

    Inital Add error handling and definitions for authentication and bot builder components
    Refactor error handling and resource management
    
    Updated solution file to modify build configurations and project mappings. Removed logging statement in `ChannelAdapter.cs`. Added `ErrorHelper.cs` to `Microsoft.Agents.BotBuilder.Errors` and `Microsoft.Agents.Authentication.Errors` namespaces. Updated `Microsoft.Agents.BotBuilder.csproj` and `Microsoft.Agents.Authentication.csproj` to include resource files. Added new `Resources.Designer.cs` and `Resources.resx` files for localized strings. Enhanced exception handling in `RestChannelServiceClientFactory.cs` and `CloudAdapter.cs` using `ExceptionParser`. Introduced `AgentErrorDefinition` and `ExceptionHelper` for detailed error definitions and exception generation.

---
+++ src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/Errors/ErrorHelper.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Agents.Core.Errors;
+
+namespace Microsoft.Agents.BotBuilder.Errors
+{
+    /// <summary>
+    /// Error helper for the Authentication core system
+    /// This is used to setup the localized error codes for the authentication subsystem of the AgentSDK
+    /// 
+    /// Note: specific auth providers are expected to implement their own error codes inside their own libraries. 
+    /// </summary>
+    internal static partial class ErrorHelper
+    {
+        /// <summary>
+        /// Base error code for the authentication provider
+        /// </summary>
+        private static readonly int baseBotBuilderErrorCode = -50000;
+
+        internal static AgentErrorDefinition NullIAccessTokenProvider = new AgentErrorDefinition(baseBotBuilderErrorCode, Properties.Resources.IAccessTokenProviderNotFound, "https://aka.ms/AgentsSDK-Error01");
+
+    }
+
+}
---
+++ src/libraries/Core/Microsoft.Agents.Core/Errors/AgentErrorDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Microsoft.Agents.Core.Errors
+{
+    /// <summary>
+    /// This class describes the error definition
+    /// </summary>
+    /// <param name="code">Error code for the exception</param>
+    /// <param name="description">Displayed Error message</param>
+    /// <param name="helplink">Help URL Link for the Error.</param>
+    public record AgentErrorDefinition(int code, string description, string helplink);
+
+    public static class ExceptionHelper
+    {
+        public static T GenerateException<T>(AgentErrorDefinition errorDefinition, Exception innerException, params string[] messageFormat) where T : Exception
+        {
+            var excp = (T)Activator.CreateInstance(typeof(T), new object[] { string.Format(errorDefinition.description, messageFormat), innerException });
+            excp.HResult = errorDefinition.code;
+            excp.HelpLink = errorDefinition.helplink;
+            return excp;
+        }
+    }
+}
