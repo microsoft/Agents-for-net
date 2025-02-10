884f5137d6b8d50a480db149e237dd7c39b98633
MattB <mattb-msft@hotmail.com>
2025-02-10 14:41:04 -0800

    Updated Error handler docs/

--- src/libraries/Core/Microsoft.Agents.Authentication/Errors/AgentAuthErrorDefinition.cs
+++ src/libraries/Core/Microsoft.Agents.Authentication/Errors/AgentAuthErrorDefinition.cs
@@ -2,6 +2,19 @@
 {
     /// <summary>
     /// This class describes the error definition, duplicated from Core as Authentication core is as standalone lib. 
+    /// 
+    /// Each Error should be created as as an AgentAuthErrorDefinition and added to the ErrorHelper class
+    /// Each definition should include an error code as a - from the base error code, a description sorted in the Resource.resx file to support localization, and a help link pointing to an AKA link to get help for the given error. 
+    /// 
+    /// 
+    /// when used, there are is 1 method in the authentication class.. 
+    /// Method 1: 
+    ///     throw new IndexOutOfRangeException(ErrorHelper.MissingAuthenticationConfiguration.description)
+    ///     {
+    ///         HResult = ErrorHelper.MissingAuthenticationConfiguration.code,
+    ///         HelpLink = ErrorHelper.MissingAuthenticationConfiguration.helplink
+    ///     };
+    ///
     /// </summary>
     /// <param name="code">Error code for the exception</param>
     /// <param name="description">Displayed Error message</param>
--- src/libraries/Core/Microsoft.Agents.Authentication/Errors/ErrorHelper.cs
+++ src/libraries/Core/Microsoft.Agents.Authentication/Errors/ErrorHelper.cs
@@ -5,8 +5,20 @@
     /// This is used to setup the localized error codes for the authentication subsystem of the AgentSDK
     /// 
     /// Note: specific auth providers are expected to implement their own error codes inside their own libraries. 
-    /// </summary>
-    internal static class ErrorHelper
+    /// 
+    /// Each Error should be created as as an AgentAuthErrorDefinition and added to the ErrorHelper class
+    /// Each definition should include an error code as a - from the base error code, a description sorted in the Resource.resx file to support localization, and a help link pointing to an AKA link to get help for the given error. 
+    /// 
+    /// when used, there are 2 methods.. 
+    /// Method 1: 
+    ///     throw new IndexOutOfRangeException(ErrorHelper.MissingAuthenticationConfiguration.description)
+    ///     {
+    ///         HResult = ErrorHelper.MissingAuthenticationConfiguration.code,
+    ///         HelpLink = ErrorHelper.MissingAuthenticationConfiguration.helplink
+    ///     };
+    /// 
+/// </summary>
+internal static class ErrorHelper
     {
         /// <summary>
         /// Base error code for the authentication provider
