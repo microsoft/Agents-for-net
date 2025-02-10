b654889aaff46b65a0fc4af6ced8c2b4932ce33a
MattB <mattb-msft@hotmail.com>
2025-02-10 14:41:04 -0800

    Updated Error handler docs/

--- src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/Errors/ErrorHelper.cs
+++ src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/Errors/ErrorHelper.cs
@@ -4,10 +4,27 @@ using Microsoft.Agents.Core.Errors;
 namespace Microsoft.Agents.BotBuilder.Errors
 {
     /// <summary>
-    /// Error helper for the Authentication core system
-    /// This is used to setup the localized error codes for the authentication subsystem of the AgentSDK
+    /// Error helper for the Agent SDK core system
+    /// This is used to setup the localized error codes for the AgentSDK
     /// 
-    /// Note: specific auth providers are expected to implement their own error codes inside their own libraries. 
+    /// Each Error should be created as as an AgentAuthErrorDefinition and added to the ErrorHelper class
+    /// Each definition should include an error code as a - from the base error code, a description sorted in the Resource.resx file to support localization, and a help link pointing to an AKA link to get help for the given error. 
+    /// 
+    /// 
+    /// when used, there are is 2 methods in used in the general space. 
+    /// Method 1: 
+    /// Throw a new exception with the error code, description and helplink
+    ///     throw new IndexOutOfRangeException(ErrorHelper.MissingAuthenticationConfiguration.description)
+    ///     {
+    ///         HResult = ErrorHelper.MissingAuthenticationConfiguration.code,
+    ///         HelpLink = ErrorHelper.MissingAuthenticationConfiguration.helplink
+    ///     };
+    ///
+    /// Method 2: 
+    /// 
+    ///     throw Microsoft.Agents.Core.Errors.ExceptionHelper.GenerateException&lt;OperationCanceledException&gt;(
+    ///         ErrorHelper.NullIAccessTokenProvider, ex, $"{BotClaims.GetAppId(claimsIdentity)}:{serviceUrl}");
+    /// 
     /// </summary>
     internal static partial class ErrorHelper
     {
--- src/libraries/Core/Microsoft.Agents.Core/Errors/AgentErrorDefinition.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Errors/AgentErrorDefinition.cs
@@ -4,6 +4,25 @@ namespace Microsoft.Agents.Core.Errors
 {
     /// <summary>
     /// This class describes the error definition
+    /// 
+    /// Each Error should be created as as an AgentAuthErrorDefinition and added to the ErrorHelper class
+    /// Each definition should include an error code as a - from the base error code, a description sorted in the Resource.resx file to support localization, and a help link pointing to an AKA link to get help for the given error. 
+    /// 
+    /// 
+    /// when used, there are is 2 methods in used in the general space. 
+    /// Method 1: 
+    /// Throw a new exception with the error code, description and helplink
+    ///     throw new IndexOutOfRangeException(ErrorHelper.MissingAuthenticationConfiguration.description)
+    ///     {
+    ///         HResult = ErrorHelper.MissingAuthenticationConfiguration.code,
+    ///         HelpLink = ErrorHelper.MissingAuthenticationConfiguration.helplink
+    ///     };
+    ///
+    /// Method 2: 
+    /// 
+    ///     throw Microsoft.Agents.Core.Errors.ExceptionHelper.GenerateException&lt;OperationCanceledException&gt;(
+    ///         ErrorHelper.NullIAccessTokenProvider, ex, $"{BotClaims.GetAppId(claimsIdentity)}:{serviceUrl}");
+    /// 
     /// </summary>
     /// <param name="code">Error code for the exception</param>
     /// <param name="description">Displayed Error message</param>
