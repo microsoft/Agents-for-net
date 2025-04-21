85c7f938d0be5b8923c832ec8e56f1de376056d7
Tracy Boehrer <trboehre@microsoft.com>
2025-04-21 15:51:39 -0500

    HandlingCards:  Matching latest Program.cs pattern

--- src/samples/HandlingCards/Program.cs
+++ src/samples/HandlingCards/Program.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+
 using HandlingCards;
 using Microsoft.Agents.Builder;
 using Microsoft.Agents.Hosting.AspNetCore;
+using Microsoft.Agents.Samples;
 using Microsoft.Agents.Storage;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -19,33 +21,35 @@ builder.Services.AddHttpClient();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 
+// Add AspNet token validation
+builder.Services.AddAgentAspNetAuthentication(builder.Configuration);
+
+// Add AgentApplicationOptions from appsettings config.
+builder.AddAgentApplicationOptions();
+
+// Add the Agent
+builder.AddAgent<CardsAgent>();
+
 // Register IStorage.  For development, MemoryStorage is suitable.
 // For production Agents, persisted storage should be used so
 // that state survives Agent restarts, and operate correctly
 // in a cluster of Agent instances.
 builder.Services.AddSingleton<IStorage, MemoryStorage>();
 
-// Add AgentApplicationOptions from config.
-builder.AddAgentApplicationOptions();
-
-// Add the Agent
-builder.AddAgent<CardsAgent>();
-
-
-// Configure the HTTP request pipeline.
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseRouting();
 app.MapPost("/api/messages", async (HttpRequest request, HttpResponse response, IAgentHttpAdapter adapter, IAgent agent, CancellationToken cancellationToken) =>
-    {
-        await adapter.ProcessAsync(request, response, agent, cancellationToken);
-    })
+{
+    await adapter.ProcessAsync(request, response, agent, cancellationToken);
+})
     .AllowAnonymous();
 
-// Setup port and listening address.
-app.Urls.Add("http://localhost:5000");
-app.Urls.Add("https://localhost:5001");
+// Hardcoded for brevity and ease of testing. 
+// In production, this should be set in configuration.
+app.Urls.Add($"http://localhost:3978");
+app.MapGet("/", () => "Microsoft Agents SDK Sample");
 
 app.Run();
+
