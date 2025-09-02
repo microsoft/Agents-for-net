d55cf9b557289bdee4086f3095161e24dfb26069
MattB <mattb-msft@hotmail.com>
2025-09-02 16:27:52 -0700

    Update Tests

--- src/tests/Microsoft.Agents.Model.Tests/AICitationTests.cs
+++ src/tests/Microsoft.Agents.Model.Tests/AICitationTests.cs
@@ -26,14 +26,14 @@ namespace Microsoft.Agents.Model.Tests
                             Url = "url",
                             Image = new AppearanceImage()
                             {
-                                Name = "image",
+                                Name = ClientCitationsIconNameEnum.Image,// "image",
                             }
                         }
                     }
                 ]
             };
 
-            var expected = "{\"@type\":\"Message\",\"@context\":\"https://schema.org\",\"@id\":\"\",\"additionalType\":[],\"citation\":[{\"position\":1,\"appearance\":{\"name\":\"name\",\"text\":\"text\",\"url\":\"url\",\"image\":{\"type\":\"ImageObject\",\"name\":\"image\"},\"@type\":\"DigitalDocument\"},\"@type\":\"Claim\"}],\"type\":\"https://schema.org/Message\"}";
+            var expected = "{\"@type\":\"Message\",\"@context\":\"https://schema.org\",\"@id\":\"\",\"additionalType\":[],\"citation\":[{\"position\":1,\"appearance\":{\"name\":\"name\",\"text\":\"text\",\"url\":\"url\",\"encodingFormat\":\"application/vnd.microsoft.card.adaptive\",\"image\":{\"type\":\"ImageObject\",\"name\":\"Image\"},\"@type\":\"DigitalDocument\"},\"@type\":\"Claim\"}],\"type\":\"https://schema.org/Message\"}";
    
             // Test serialize
             var jsonOut = ProtocolJsonSerializer.ToJson(entityOut);
