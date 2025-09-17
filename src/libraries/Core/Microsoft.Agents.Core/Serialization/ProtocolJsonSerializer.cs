576f02ce2b39eaf4a2b66f51724d2ac61df2ec2e
Alex S <olstakh@microsoft.com>
2025-09-17 13:45:19 -0700

    Add assembly level attribute

--- src/libraries/Core/Microsoft.Agents.Core/Serialization/ProtocolJsonSerializer.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Serialization/ProtocolJsonSerializer.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Agents.Core.Serialization
 
         static ProtocolJsonSerializer()
         {
-            SerializationInitAttribute.InitSerialization();
+            SerializationInitAssemblyAttribute.InitSerialization();
         }
 
         private static JsonSerializerOptions CreateConnectorOptions()
