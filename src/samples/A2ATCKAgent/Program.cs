eff73a194776cf4561662103b42cf17f85ce6580
Tracy Boehrer <trboehre@microsoft.com>
2026-03-19 08:36:35 -0500

    Corrected build errors in A2ATCKAgent

--- src/samples/A2ATCKAgent/Program.cs
+++ src/samples/A2ATCKAgent/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using A2A;
 using A2A.AspNetCore;
 using A2ATCKAgent;
 using Microsoft.Agents.Builder;
@@ -13,35 +12,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-using System;
 using System.Threading;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Logging.AddConsole();
-
-builder.Services.AddHttpClient()
-    .AddLogging()
-    .AddOpenTelemetry()
-    .ConfigureResource(resource =>
-    {
-        resource.AddService("A2ATCKAgent");
-    })
-    .WithTracing(tracing => tracing
-        .AddSource(TaskManager.ActivitySource.Name)
-        .AddSource(A2AJsonRpcProcessor.ActivitySource.Name)
-        .AddSource(A2AHttpProcessor.ActivitySource.Name)
-        .AddAspNetCoreInstrumentation()
-        .AddHttpClientInstrumentation()
-        .AddOtlpExporter(options =>
-        {
-            options.Endpoint = new Uri("http://localhost:4317");
-            options.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
-        })
-     );
+builder.Services.AddHttpClient();
 
 // Add AgentApplicationOptions from appsettings section "AgentApplication".
 builder.AddAgentApplicationOptions();
