ed28279689166d0e7b2be109d76e898d9c0ba5b9
Rodrigo Brandao <robrandao@microsoft.com>
2026-05-21 10:16:10 -0700

    Another commit

--- src/libraries/Extensions/Microsoft.Agents.Extensions.Teams/TeamsTurnContext.cs
+++ src/libraries/Extensions/Microsoft.Agents.Extensions.Teams/TeamsTurnContext.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.Agents.Builder;
+using Microsoft.Agents.Builder.App;
+using Microsoft.Agents.Builder.App.Proactive;
 using Microsoft.Agents.Core.Models;
 using System.Runtime.CompilerServices;
 using System.Security.Claims;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Agents.Authentication;
+using Microsoft.Agents.Builder.State;
+using Microsoft.Agents.Core.Errors;
+using Microsoft.Agents.Core.Serialization;
+using Microsoft.Agents.Extensions.Teams;
+using Microsoft.Agents.Extensions.Teams.TeamsChannels;
+using Microsoft.Agents.Extensions.Teams.TeamsTeams;
+using System.Xml;
 
 namespace Microsoft.Agents.Extensions.Teams
 {
@@ -11,88 +22,145 @@ namespace Microsoft.Agents.Extensions.Teams
     {
 
         private readonly ITurnContext _turnContext;
+        private readonly Proactive _proactive;
 
-        IChannelAdapter Adapter
+        #region ITurnContext Contract
+
+        public IChannelAdapter Adapter
         {
             get { return _turnContext.Adapter; }
         }
 
-        TurnContextStateCollection Services
+        public TurnContextStateCollection Services
         {
             get {  return _turnContext.Services; }
         }
 
-        TurnContextStateCollection StackState
+        public TurnContextStateCollection StackState
         {
             get { return _turnContext.StackState; }
         }
 
-        IStreamingResponse StreamingResponse
+        public IActivity Activity
+        {
+            get { return _turnContext.Activity; }
+        }
+
+        public IStreamingResponse StreamingResponse
         {
             get { return _turnContext.StreamingResponse; }
         }
 
-        bool Responses
+        public bool Responded
         {
             get { return _turnContext.Responded; }
         }
 
-        ClaimsIdentity Identity
+        public ClaimsIdentity Identity
         {
             get { return _turnContext.Identity; }
         }
 
-        Task<ResourceResponse> SendActivityAsync(string text, string speak = null, string inputHint = "acceptingInput", CancellationToken cancellationToken = default)
+        public TeamsTurnContext(ITurnContext turnContext, Proactive proactive)
+        {
+            this._turnContext = turnContext;
+            this._proactive = proactive;
+        }
+
+        public Task<ResourceResponse> SendActivityAsync(string text, string speak = null, string inputHint = "acceptingInput", CancellationToken cancellationToken = default)
         {
             return _turnContext.SendActivityAsync(text, speak, inputHint, cancellationToken);
         }
 
-        Task<ResourceResponse> SendActivityAsync(IActivity activity, CancellationToken cancellationToken = default)
+        public Task<ResourceResponse> SendActivityAsync(IActivity activity, CancellationToken cancellationToken = default)
         {
             return _turnContext.SendActivityAsync(activity, cancellationToken);
         }
 
-        Task<ResourceResponse> UpdateActivityAsync(IActivity activity, CancellationToken cancellationToken = default)
+        public Task<ResourceResponse[]> SendActivitiesAsync(IActivity[] activities, CancellationToken cancellationToken = default)
+        {
+            return _turnContext.SendActivitiesAsync(activities, cancellationToken);
+        }
+
+        public Task<ResourceResponse> UpdateActivityAsync(IActivity activity, CancellationToken cancellationToken = default)
         {
             return _turnContext.UpdateActivityAsync(activity, cancellationToken);
         }
 
-        Task DeleteActivityAsync(string activityId, CancellationToken cancellationToken = default)
+        public Task DeleteActivityAsync(string activityId, CancellationToken cancellationToken = default)
         {
             return _turnContext.DeleteActivityAsync(activityId, cancellationToken);
         }
 
-        Task DeleteActivityAsync(ConversationReference conversationReference, CancellationToken cancellationToken = default)
+        public Task DeleteActivityAsync(ConversationReference conversationReference, CancellationToken cancellationToken = default)
         {
             return _turnContext.DeleteActivityAsync(conversationReference, cancellationToken);
         }
 
-        TeamsTurnContext OnSendActivities(SendActivitiesHandler handler)
+        public ITurnContext OnSendActivities(SendActivitiesHandler handler)
         {
             _turnContext.OnSendActivities(handler);
             return this;
         }
 
-        TeamsTurnContext OnUpdateActivity(UpdateActivityHandler handler)
+        public ITurnContext OnUpdateActivity(UpdateActivityHandler handler)
         {
             _turnContext.OnUpdateActivity(handler);
             return this;
         }
 
-        TeamsTurnContext OnDeleteActivity(DeleteActivityHandler handler)
+        public ITurnContext OnDeleteActivity(DeleteActivityHandler handler)
         {
             _turnContext.OnDeleteActivity(handler);
             return this;
         }
 
-        Task<ResourceResponse> TraceActivityAsync(string name, object value = null, string valueType = null, [CallerMemberName] string label = null, CancellationToken cancellationToken = default)
+        public Task<ResourceResponse> TraceActivityAsync(string name, object value = null, string valueType = null, [CallerMemberName] string label = null, CancellationToken cancellationToken = default)
         {
             return _turnContext.TraceActivityAsync(name, value, valueType, label, cancellationToken);
         }
 
-        public TeamsTurnContext(ITurnContext turnContext)
+        #endregion
+
+        private async Task<ResourceResponse> SendActivityToUserAsync(Microsoft.Teams.Api.Account userAccount, IActivity activity, CancellationToken cancellationToken = default)
         {
-            this._turnContext = turnContext;
+            var createOptions = CreateConversationOptionsBuilder
+                .Create(Identity.GetIncomingAudience(), Channels.Msteams, Activity.ServiceUrl)
+                .WithUser(userAccount.ToCoreChannelAccount())
+                .WithTenantId(Activity.Conversation.TenantId)
+                .IsGroup(false)
+                .Build();
+            await _proactive.CreateConversationAsync(
+                    Adapter,
+                    createOptions,
+                    async (ctx, ts, ct) =>
+                    {
+                        await ctx.SendActivityAsync(activity, ct);
+                    },
+                    cancellationToken: cancellationToken);
+            return new ResourceResponse();
         }
-    }
+
+        public Task<ResourceResponse> SendActivityAsync(Microsoft.Teams.Api.Account userAccount, IActivity activity, CancellationToken cancellationToken = default)
+        {
+            return SendActivityToUserAsync(userAccount, activity, cancellationToken);
+        }
+
+        public Task<ResourceResponse> SendActivityAsync(string conversationId, IActivity activity, CancellationToken cancellationToken = default)
+        {
+            return _proactive.SendActivityAsync(
+                Adapter,
+                conversationId,
+                activity,
+                cancellationToken: cancellationToken);
+        }
+
+        public Task<ResourceResponse> ReplyAsync(IActivity activity, CancellationToken cancellationToken = default)
+        {
+            
+        }
+         public Task<ResourceResponse> ReplyAsync(string text, string speak = null, string inputHint = "acceptingInput", CancellationToken cancellationToken = default)
+        {
+            return _turnContext.ReplyAsync(text, speak, inputHint, cancellationToken);
+        }
 }
