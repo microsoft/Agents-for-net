340722523f42b1c646a73138cae08a9fa523823c
tracyboehrer <tracy.boehrer@pm.me>
2026-08-31 11:13:16 -0500

    Add some TCK SUT test handling in A2ATCKAgent

--- src/samples/A2ATCKAgent/MyAgent.cs
+++ src/samples/A2ATCKAgent/MyAgent.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Agents.Builder;
+using A2A;
 using Microsoft.Agents.Builder.App;
 using Microsoft.Agents.Builder.State;
 using Microsoft.Agents.Extensions.A2A;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,8 +20,27 @@ public partial class MyAgent : AgentApplication
         A2AExtension.OnMessage(OnA2AMessageAsync);
     }
 
-    private Task OnA2AMessageAsync(IA2ATurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+    private async Task OnA2AMessageAsync(IA2ATurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
     {
-        return turnContext.SendActivityAsync($"You sent an A2A message with text: '{turnContext.Activity.Text}'", cancellationToken: cancellationToken);
+        // Reference: a2a-tck\sut\a2a-python\sut_agent.py
+
+        var message = turnContext.Activity.ChannelData;
+
+        if (message?.MessageId.StartsWith("tck-artifact-text") == true)
+        {
+            var taskUpdater = new TaskUpdater(turnContext.Client.EventQueue, message.TaskId!, message.ContextId!);
+            await taskUpdater.AddArtifactAsync([Part.FromText("Generated text content")], cancellationToken: cancellationToken);
+            await taskUpdater.CompleteAsync(cancellationToken: cancellationToken);
+        }
+        else if (message?.MessageId.StartsWith("tck-artifact-file") == true)
+        {
+            var taskUpdater = new TaskUpdater(turnContext.Client.EventQueue, message.TaskId!, message.ContextId!);
+            await taskUpdater.AddArtifactAsync([new Part { Raw = Encoding.UTF8.GetBytes("tck"), MediaType = "text/plain", Filename = "output.txt" }], cancellationToken: cancellationToken);
+            await taskUpdater.CompleteAsync(cancellationToken: cancellationToken);
+        }
+        else
+        {
+            await turnContext.SendActivityAsync($"You sent an A2A message with text: '{turnContext.Activity.Text}'", cancellationToken: cancellationToken);
+        }
     }
 }
